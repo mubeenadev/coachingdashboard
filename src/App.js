16fd741a7f980b4aa3d7db9138c8fb43fef9e27a
mubeena <mubeenahamza@gmail.com>
2023-09-19 14:45:34 +0200

    Configuration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ChakraProvider, Flex, theme } from "@chakra-ui/react";
+import { ColorModeSwitcher } from "./ColorModeSwitcher";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import LoginPage from "./Pages/LoginPage";
+import SignUpPage from "./Pages/SignUpPage";
+import ResetPassword from "./Pages/ResetPassword";
+import Link from "./components/Link";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ChakraProvider theme={theme}>
+            <Router>
+                <Flex
+                    borderBottom="1px solid black"
+                    justify="space-between"
+                    fontSize="xl"
+                >
+                    <Link to="/">Home</Link>
+                    <Link to="/login">Login</Link>
+                    <Link to="/signup">SignUp</Link>
+                    <Link to="/reset-password">Reset Password</Link>
+                    <ColorModeSwitcher justifySelf="flex-end" />
+                </Flex>
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route exact path="/login" element={<LoginPage />} />
+                    <Route exact path="/signup" element={<SignUpPage />} />
+                    <Route
+                        exact
+                        path="/reset-password"
+                        element={<ResetPassword />}
+                    />
+                </Routes>
+            </Router>
+        </ChakraProvider>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ColorModeScript } from "@chakra-ui/react";
+import React, { StrictMode } from "react";
+import * as ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <StrictMode>
+        <ColorModeScript />
+        <App />
+    </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
