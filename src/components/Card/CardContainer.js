f9c9272f0df9dc80098fa1d0ab375bff3b193bf1
mubeena <mubeenahamza@gmail.com>
2023-09-26 13:38:21 +0200

    Add Card component

---
+++ src/components/Card/CardContainer.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Stack, Text } from "@chakra-ui/react";
+
+function CardConatainer(props) {
+    const { variant, children, ...rest } = props;
+
+    return (
+        <Stack
+            maxW="360px"
+            justifyContent="center"
+            alignItems="center"
+            bg="white"
+            p={3}
+            borderRadius={24}
+            boxShadow="0px 8px 16px rgba(58, 58, 68, 0.12), 0px 16px 32px rgba(90, 91, 106, 0.12);"
+        >
+            <Text
+                fontSize="2xl"
+                fontWeight={700}
+                lineHeight="150%"
+                w="100%"
+                ml={2}
+                mb={2}
+            >
+                {props.title}
+            </Text>
+
+            {children}
+        </Stack>
+    );
+}
+
+export default CardConatainer;
