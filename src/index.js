60232a955050b2ac44505cb21d331c68c39195d8
mubeena <mubeenahamza@gmail.com>
2023-09-26 13:40:19 +0200

    Add Html editor

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import React, { StrictMode } from "react";
 import * as ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "./index.css";
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 
 root.render(
-    <StrictMode>
+    <>
         <ColorModeScript />
         <App />
-    </StrictMode>
+    </>
 );
 
 // If you want to start measuring performance in your app, pass a function
