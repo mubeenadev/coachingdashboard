e2ae70e07fc7d2d3a0e2fe98c0abf39822b84fe3
mubeena <mubeenahamza@gmail.com>
2023-10-03 17:36:14 +0200

    Add client module

--- src/components/Card/Client/ClientCard.js
+++ src/components/Card/Client/ClientCard.js
@@ -1,62 +1,85 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import CardContainer from "../CardContainer";
 import { VStack, Box } from "@chakra-ui/react";
-import {
-    Text,
-    Flex,
-    Spacer,
-    Button,
-    Icon,
-    Link,
-    HStack,
-    Stack,
-} from "@chakra-ui/react";
-
-const Client = [
-    {
-        name: "Sally",
-        coach_category: "Job",
-        type: "AVG",
-    },
-    {
-        name: "Mika",
-        coach_category: "Business",
-        type: "Private",
-    },
-    {
-        name: "Blippi",
-        coach_category: "Job",
-        type: "AVG",
-    },
-    {
-        name: "Jolly",
-        coach_category: "Career change",
-        type: "AVG",
-    },
-    {
-        name: "Sally",
-        coach_category: "Job",
-        type: "AVG",
-    },
-    {
-        name: "Mika",
-        coach_category: "Business",
-        type: "Private",
-    },
-    {
-        name: "Blippi",
-        coach_category: "Job",
-        type: "AVG",
-    },
-    {
-        name: "Jolly",
-        coach_category: "Career change",
-        type: "AVG",
-    },
-];
+import { Text, Flex, Spacer, Button, Link } from "@chakra-ui/react";
+import ClientModal from "./ClientModal";
+import { query, collection, getDocs, where, addDoc } from "firebase/firestore";
+import { auth, db } from "../../../Config/Firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const ClientCard = () => {
-    const data = Client;
+    const [clientList, setClientList] = useState([]);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [user, loading] = useAuthState(auth);
+
+    //fetch the client list
+    const Fetchdata = async () => {
+        try {
+            const coachId = user.uid;
+
+            const querySnapshot = query(
+                collection(db, "join-coach-coachee"),
+                where("coachId", "==", coachId)
+            );
+
+            const doc = await getDocs(querySnapshot);
+            let userList = [];
+            doc.forEach((userDoc) => {
+                userList.push(userDoc.data());
+            });
+            const userInfo = [];
+
+            await Promise.all(
+                userList.map(async (user) => {
+                    const q = query(
+                        collection(db, "users"),
+                        where("uid", "==", user.clientId)
+                    );
+                    const docSnapshot = await getDocs(q);
+
+                    if (!docSnapshot.empty) {
+                        const userData = docSnapshot.docs[0].data();
+
+                        userInfo.push({
+                            name: userData.name,
+                            type: user.coachCategory,
+                            uid: userData.uid,
+                            email: userData.email,
+                        });
+                    }
+                })
+            );
+            setClientList(userInfo);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    };
+
+    useEffect(() => {
+        if (!loading && user) {
+            Fetchdata();
+        }
+    }, [loading, user]);
+
+    const handleModalOpen = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleModalClose = () => {
+        setIsModalOpen(false);
+    };
+    const handleSaveData = async (newClient) => {
+        try {
+            const docRef = await addDoc(
+                collection(db, "join-coach-coachee"),
+                newClient
+            );
+            Fetchdata();
+        } catch (e) {
+            console.error("Error adding document: ", e);
+        }
+    };
+
     return (
         <CardContainer title="Clients">
             <VStack width={"100%"} p={5} maxHeight={300}>
@@ -68,52 +91,62 @@ const ClientCard = () => {
                         overflowY: "auto", // Enable vertical scrolling
                     }}
                 >
-                    {data.map((row, index) => {
-                        return (
-                            <Flex
-                                key={index}
-                                width={"100%"}
-                                alignItems="center"
-                            >
-                                <Stack>
-                                    <Text
-                                        fontSize="md"
-                                        color={"gray.700"}
-                                        fontWeight="semibold"
-                                    >
-                                        {row.name}
-                                    </Text>
-                                    <Text
-                                        fontSize="sm"
-                                        color="gray.400"
-                                        fontWeight="semibold"
-                                        me="16px"
-                                    >
-                                        {row.coach_category}
-                                    </Text>
-                                </Stack>
-                                <Spacer />
-                                <Link href="#" isExternal>
-                                    <Button
-                                        p="0px"
-                                        bg="transparent"
-                                        variant="no-hover"
-                                    >
-                                        <Flex alignItems="center" p="12px">
-                                            <Text
-                                                fontSize="md"
-                                                color={"gray.700"}
-                                                fontWeight="bold"
-                                            >
-                                                Open
-                                            </Text>
-                                        </Flex>
-                                    </Button>
-                                </Link>
-                            </Flex>
-                        );
-                    })}
+                    {clientList &&
+                        clientList.map((row, index) => {
+                            return (
+                                <Flex
+                                    key={index}
+                                    width={"100%"}
+                                    alignItems="center"
+                                    marginBottom={15}
+                                >
+                                    <Flex direction="column">
+                                        <Text
+                                            fontSize="md"
+                                            color={"gray.700"}
+                                            fontWeight="semibold"
+                                        >
+                                            {row.name}
+                                        </Text>
+                                        <Text
+                                            fontSize="sm"
+                                            color="gray.400"
+                                            fontWeight="semibold"
+                                            me="16px"
+                                        >
+                                            {row.email}
+                                        </Text>
+                                    </Flex>
+                                    <Spacer />
+                                    <Link href="#" isExternal>
+                                        <Button
+                                            p="0px"
+                                            bg="transparent"
+                                            variant="no-hover"
+                                        >
+                                            <Flex alignItems="center" p="12px">
+                                                <Text
+                                                    fontSize="md"
+                                                    color={"gray.700"}
+                                                    fontWeight="bold"
+                                                >
+                                                    Open
+                                                </Text>
+                                            </Flex>
+                                        </Button>
+                                    </Link>
+                                </Flex>
+                            );
+                        })}
                 </Box>
+                <Button onClick={handleModalOpen} p={2}>
+                    Add New Client
+                </Button>
+                <ClientModal
+                    isOpen={isModalOpen}
+                    onClose={handleModalClose}
+                    onSave={handleSaveData}
+                />
             </VStack>
         </CardContainer>
     );
---
+++ src/components/Card/Client/ClientModal.js
@@ -0,0 +1,136 @@
+import React, { useState, useEffect } from "react";
+import {
+    Button,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalCloseButton,
+    ModalBody,
+    ModalFooter,
+    FormControl,
+    FormErrorMessage,
+    FormLabel,
+    Input,
+    Select,
+    Spinner,
+} from "@chakra-ui/react";
+import { auth, db } from "../../../Config/Firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { collection, query, where, getDocs } from "firebase/firestore";
+const ClientModal = ({ isOpen, onClose, onSave }) => {
+    const initialRef = React.useRef();
+    const finalRef = React.useRef();
+
+    const [clientEmail, setClientEmail] = useState("");
+    const [coachCategory, setCoachCategory] = useState("");
+    const [user, loading] = useAuthState(auth);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
+    const handleSave = async () => {
+        setIsLoading(true);
+        const clientId = await checkClientEmail();
+        setIsLoading(false);
+
+        if (clientId !== "") {
+            onSave({
+                clientId,
+                coachCategory,
+                coachId: user.uid, //coach user id
+            });
+        }
+    };
+
+    //check client email exist
+
+    const checkClientEmail = async () => {
+        const q = query(
+            collection(db, "users"),
+            where("email", "==", clientEmail),
+            where("userType", "==", "coachee")
+        );
+        const querySnapshot = await getDocs(q);
+
+        if (querySnapshot.empty) {
+            setIsError(true);
+            return;
+        } else {
+            const doc = querySnapshot.docs[0].data();
+            if (doc) {
+                setIsError(false);
+                onClose();
+                return doc.uid;
+            }
+        }
+    };
+
+    return (
+        <Modal
+            initialFocusRef={initialRef}
+            finalFocusRef={finalRef}
+            isOpen={isOpen}
+            onClose={onClose}
+        >
+            <ModalOverlay />
+            <ModalContent>
+                <ModalHeader>Add New Client</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody pb={6}>
+                    {isLoading ? (
+                        <Spinner
+                            thickness="4px"
+                            speed="0.65s"
+                            emptyColor="gray.200"
+                            color="blue.500"
+                            size="xl"
+                        />
+                    ) : (
+                        <>
+                            <FormControl isInvalid={isError}>
+                                <FormLabel>Email</FormLabel>
+                                <Input
+                                    ref={initialRef}
+                                    placeholder="Title"
+                                    value={clientEmail}
+                                    onChange={(e) => {
+                                        const email = e.target.value;
+                                        setClientEmail(email);
+                                        setIsError(false);
+                                    }}
+                                />
+                                {isError && (
+                                    <FormErrorMessage>
+                                        User with email " {clientEmail} " does
+                                        not exist
+                                    </FormErrorMessage>
+                                )}
+                            </FormControl>
+                            <FormControl mt={4}>
+                                <FormLabel>Category</FormLabel>
+                                <Select
+                                    placeholder="Select Coach Category"
+                                    value={coachCategory}
+                                    onChange={(e) =>
+                                        setCoachCategory(e.target.value)
+                                    }
+                                >
+                                    <option value="AVG">AVG</option>
+                                    <option value="private">Private</option>
+                                </Select>
+                            </FormControl>
+                        </>
+                    )}
+                </ModalBody>
+
+                <ModalFooter>
+                    <Button colorScheme="blue" mr={3} onClick={handleSave}>
+                        Save
+                    </Button>
+                    <Button onClick={onClose}>Cancel</Button>
+                </ModalFooter>
+            </ModalContent>
+        </Modal>
+    );
+};
+
+export default ClientModal;
